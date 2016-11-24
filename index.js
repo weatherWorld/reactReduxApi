697fb61968656476a95ec4ea6299196a465c3bd2
bradymat <bradymat@gmail.com>
2016-11-25 10:50:05 +1300

    setop reducer, starte API call

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app'
+import redux from 'redux'
+import clone from 'clone'
+import request from 'superagent'
+
+import reducer from '/reducer/reducer'
+
+const initialState = {
+
+}
+
+const store = redux.createStore(reducer, state)
+
+store.subscribe( () => {
+  var state = store.getState
+})
+
+const url = "api.openweathermap.org/data/2.5/forecast/city?id=2179538&APPID=44237abc5b450fad36fefd2fca9535d5"
+
+request
+  .get(url)
+  .end((err, res) => {
+    if (err) {
+      console.log("Cannot find weather", err)
+    } else {
+      store.dispatch({type: "GET_WEATHER", payload: res.body})
+    }
+  })
+
 
 render(<App name='weatherWorld' />, document.querySelector('main'))
 console.log('welcome to weatherWorld')
