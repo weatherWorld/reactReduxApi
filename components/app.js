8be373756050103b3aa457cd4a68f77eb98e3c5d
Simon Casey <simon.jk.casey@gmail.com>
2016-11-25 16:20:23 +1300

    timed cycle through cities

--- components/app.js
+++ components/app.js
@@ -5,19 +5,21 @@ const App = (props) =>
     <h1>{props.name}</h1>
     <button onClick={props.getWeather}>Get Weather</button>
     <div>
-      <p>{props.state.city.name}</p>
+      <p>City Name: {props.state.city.name}</p>
       <p>Temperature: {props.state.city.temp}</p>
       <p>Weather: {props.state.city.description}</p>
     </div>
 
-    <form>
+     {/* <form>
       <select name = 'cityDropdown'>
         <option value = '' disabled>City</option>
-          {props.state.city.map(function(item){
+          {for (var i = 0; i < array.length; i++) {
+            array[i]
+          }
             return <option value={item.name}>{item.name}</option>
-          })}
+          }
       </select>
-    </form>
+    </form> */}
   </div>
 
 export default App
--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import request from 'superagent'
 import App from './components/app'
 const reducer = require ('./reducer/reducer')
 
-const initialState = {}
+const initialState = {
+  city:[]
+}
 
 const store = createStore(reducer, initialState)
 
@@ -19,22 +21,29 @@ store.subscribe( () => {
 
 function getWeather(){
   const cities = [2179538, 1053507, 6547384, 2853773, 2138410, 3523127, 1727087, 3146160, 4676032, 4160100, 4235766, 4389658, 3124963, 3176441, 7839404, 2766495, 5059836, 7290568, 158179, 6551604, 2511091, 6440879, 3176438]
-  for(var i=0; i < cities.length; i++) {
-    const url = "http://api.openweathermap.org/data/2.5/forecast/city?id="+cities[i]+"&APPID=44237abc5b450fad36fefd2fca9535d5"
-    request
-    .get(url)
-    .end((err, res) => {
-      if (err) {
-        return err
-      } else {
-        var name = res.body.city.name
-        var temp = res.body.list[i].main.temp
-        var description = res.body.list[i].weather[0].description
-        console.log('LOOK HERE', name, temp, description)
-        store.dispatch({type: "GOT_WEATHER", payload: {name, temp, description}})
+  slowDownLoop()
+  function slowDownLoop() {
+    for(var i=0; i < cities.length; i++) {
+      (function(i){
+        setTimeout(function(){
+      const url = "http://api.openweathermap.org/data/2.5/forecast/city?id="+cities[i]+"&APPID=44237abc5b450fad36fefd2fca9535d5"
+      request
+      .get(url)
+      .end((err, res) => {
+        if (err) {
+          return err
+        } else {
+          var name = res.body.city.name
+          var temp = res.body.list[i].main.temp
+          var description = res.body.list[i].weather[0].description
+          console.log('LOOK HERE', name, temp, description)
+          store.dispatch({type: "GOT_WEATHER", payload: {name, temp, description}})
       }
     })
-  }
+  }, i*3000)
+}(i))
+}
+}
 }
 
 store.dispatch({type:'INIT'})
